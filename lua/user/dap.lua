46359a88b61092eddfadd33ec0c1bf891c731d9a
isaaczhou <isaaczhou85@gmail.com>
2022-12-10 11:12:49 -0500

    update'

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -35,37 +35,59 @@ dap.configurations.python = {
 	},
 }
 
--- cpp dap
-dap.adapters.cppdbg = {
-	id = "cppdbg",
+-- lldb
+dap.adapters.lldb = {
 	type = "executable",
-	command = "/home/isaac/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+	-- command = "/home/isaac/.local/share/nvim/mason/packages/codelldb/codelldb",
+	command = "/usr/bin/lldb",
+	name = "lldb",
 }
 
 dap.configurations.cpp = {
 	{
-		name = "Launch file",
-		type = "cppdbg",
+		name = "Launch",
+		type = "lldb",
 		request = "launch",
 		program = function()
 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 		end,
 		cwd = "${workspaceFolder}",
-		stopAtEntry = true,
-	},
-	{
-		name = "Attach to gdbserver :1234",
-		type = "cppdbg",
-		request = "launch",
-		MIMode = "gdb",
-		miDebuggerServerAddress = "localhost:1234",
-		miDebuggerPath = "/usr/bin/gdb",
-		cwd = "${workspaceFolder}",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
+		stopOnEntry = false,
+		args = {},
 	},
 }
+
+-- cpp dap
+-- dap.adapters.cppdbg = {
+-- 	id = "cppdbg",
+-- 	type = "executable",
+-- 	command = "/home/isaac/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+-- }
+--
+-- dap.configurations.cpp = {
+-- 	{
+-- 		name = "Launch file",
+-- 		type = "cppdbg",
+-- 		request = "launch",
+-- 		program = function()
+-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+-- 		end,
+-- 		cwd = "${workspaceFolder}",
+-- 		stopAtEntry = true,
+-- 	},
+-- 	{
+-- 		name = "Attach to gdbserver :1234",
+-- 		type = "cppdbg",
+-- 		request = "launch",
+-- 		MIMode = "gdb",
+-- 		miDebuggerServerAddress = "localhost:1234",
+-- 		miDebuggerPath = "/usr/bin/gdb",
+-- 		cwd = "${workspaceFolder}",
+-- 		program = function()
+-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+-- 		end,
+-- 	},
+-- }
 
 local dap_ui_status_ok, dapui = pcall(require, "dapui")
 if not dap_ui_status_ok then
